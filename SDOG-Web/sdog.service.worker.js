c3ede0c75f1b101b7b399d6e8e226523cf297c56
Sean Maida <smaida2002@yahoo.com>
2025-04-29 21:32:06 -0400

    updated

--- SDOG-Web/sdog.service.worker.js
+++ SDOG-Web/sdog.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745976451|1001731010';
+const CACHE_VERSION = '1745976683|1233083920';
 /** @type {string} */
 const CACHE_PREFIX = 'Stevens Day of G-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
